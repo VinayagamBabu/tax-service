167e9949d2f9293979281fb575ef50c4765faa4f
VinayagamBabu <vinayrajanbabu@gmail.com>
2022-10-05 20:28:12 +0530

    Spring boot changes

--- src/main/java/com/mycompany/TaxService/configuration/MyConfiguration.java
+++ src/main/java/com/mycompany/TaxService/configuration/MyConfiguration.java
@@ -1,5 +1,8 @@
 package com.mycompany.TaxService.configuration;
 
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
